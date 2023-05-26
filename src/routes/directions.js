6c07312c79eeafd1fbed1a447d3833c2c64f8b79
Rodrigo Balbino <rodrigo080599@hotmail.com>
2023-05-26 01:07:08 -0300

    Add images and routes
    Co-authored-by: antonioaldisio <aldisiofilho@gmail.com>

--- src/routes/directions.js
+++ src/routes/directions.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from '../views/homepage/index'
+import Motivation from '../views/secondePage/index'
+import Schedule from '../views/thirdPage/index'
 import React from 'react'
 
 function Directions() {
@@ -8,6 +10,8 @@ function Directions() {
     <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/Motivation" element={<Motivation />} />
+          <Route path="/Schedule" element={<Schedule />} />
         </Routes>
     </BrowserRouter>
   )
